00e23b6edcf5e23b8e0837808af86c66af478f87
Kjetil Rønhovde <kjetil.ronhovde@no.experis.com>
2023-03-01 13:13:23 +0100

    fix problem with get
    Co-authored-by: Annerik <Annerik@users.noreply.github.com>

--- pokemon-trainer/src/app/services/pokemon-catalogue.service.ts
+++ pokemon-trainer/src/app/services/pokemon-catalogue.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { finalize } from 'rxjs';
+import { finalize, map } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Pokemon } from '../models/pokemon.model';
+import { PokemonAPI } from '../models/pokemonAPI.model';
 const {apiPokemons } = environment;
 
 @Injectable({
@@ -10,9 +11,10 @@ const {apiPokemons } = environment;
 })
 export class PokemonCatalogueService {
 
-  _pokemons: Pokemon[]= [];
+  private _pokemons: Pokemon[]= [];
   private _error: string = "";
   private _loading: boolean = false;
+  private _apiDone = false;
 
   get pokemons(): Pokemon[] {
     return this._pokemons
@@ -26,25 +28,58 @@ export class PokemonCatalogueService {
     return this._loading;
   }
 
-  constructor(private readonly http: HttpClient) { }
-
-  public findAllPokemons(): void {
-    this._loading = true;
-    this.http.get<Pokemon[]>(apiPokemons)
-    .pipe(
-      finalize(() => {
-        this._loading = false;
-      })
-    )
-    .subscribe({
-      next: (pokemons: Pokemon[]) => {
-        this._pokemons = pokemons;
-
-      },
-      error: (error: HttpErrorResponse) => {
-        this._error = error.message
-
-      }
-    })
+  constructor(private readonly http: HttpClient) { 
+    this._pokemons = [];
   }
+
+
+  public findAllPokemons(): void{
+    if(!this._apiDone){
+      this._loading = true;
+      this.http.get<PokemonAPI>(apiPokemons)
+      .pipe(
+        map((response: PokemonAPI) => response.results)
+      )
+      .pipe(
+        finalize(() => {
+          this._loading = false;
+        })
+      )
+      .subscribe(
+        (results: Pokemon[]) => {
+          for(let result of results) {
+            this.pokemons.push(result)
+          }
+        }
+      ),{
+        error: (error: HttpErrorResponse) => {
+        }
+      }
+    }
+    this._apiDone = true;
+  }
+
+
+
+
+  // public findAllPokemons(): void {
+  //   this._loading = true;
+  //   this.http.get<Pokemon[]>(apiPokemons)
+  //   .pipe(
+  //     finalize(() => {
+  //       this._loading = false;
+  //     })
+  //   )
+  //   .subscribe({
+  //     next: (pokemons: Pokemon[]) => {
+  //       if (pokemons && pokemons.length > 0) {
+  //         this.pokemons.push(pokemons[0]);
+  //       }
+  //       // console.log(pokemons)
+  //     },
+  //     error: (error: HttpErrorResponse) => {
+  //       this._error = error.message;
+  //     }
+  //   })
+  // }
 }

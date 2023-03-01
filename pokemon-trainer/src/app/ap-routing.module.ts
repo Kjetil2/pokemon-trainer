e8b5fce50be748bc46e4f365d2198deaba32fb1d
Kjetil Rønhovde <kjetil.ronhovde@no.experis.com>
2023-03-01 08:02:10 +0100

    updated functionality

---
+++ pokemon-trainer/src/app/ap-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./guards/auth.guard";
+import { CatalogueComponent } from "./pages/catalogue/catalogue.component";
+import { LoginPage} from "./pages/login/login.page";
+import { TrainerComponent } from "./pages/trainer/trainer.component";
+
+const routes: Routes = [
+    {
+        path:"",
+        pathMatch:"full",
+        redirectTo:"/login"
+    },
+    {
+        path:"login",
+        component: LoginPage
+    },
+    {
+        path:"trainer",
+        component: TrainerComponent,
+        canActivate:[ AuthGuard ]
+    },
+    {
+        path:"catalogue",
+        component: CatalogueComponent,
+        canActivate:[ AuthGuard ]
+    }
+
+]
+
+@NgModule({
+imports: [
+    RouterModule.forRoot(routes)
+],
+exports: [
+    RouterModule
+]
+})
+export class AppRoutingMoudle{
+
+}
--- pokemon-trainer/src/app/app.module.ts
+++ pokemon-trainer/src/app/app.module.ts
@@ -1,14 +1,31 @@
+import { AppRoutingMoudle } from './ap-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { LoginPage } from './pages/login/login.page';
+import { TrainerComponent } from './pages/trainer/trainer.component';
+import { CatalogueComponent } from './pages/catalogue/catalogue.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,   
+    LoginPage,
+    TrainerComponent,
+    CatalogueComponent,
+    LoginFormComponent,
+    PokemonListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingMoudle
   ],
   providers: [],
   bootstrap: [AppComponent]

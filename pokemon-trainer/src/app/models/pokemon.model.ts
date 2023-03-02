e15fd73df784d369f27235089014ccfe146c7334
Kjetil Rønhovde <kjetil.ronhovde@no.experis.com>
2023-03-02 18:55:47 +0100

    fix: updated properties

--- pokemon-trainer/src/app/models/pokemon.model.ts
+++ pokemon-trainer/src/app/models/pokemon.model.ts
@@ -1,5 +1,6 @@
 export interface Pokemon {
-    id: number;
     name: string;
     pokemonimg: string;
+    url: string;
+    
 }
--- pokemon-trainer/src/app/models/user.model.ts
+++ pokemon-trainer/src/app/models/user.model.ts
@@ -1,6 +1,8 @@
+import { Pokemon } from "./pokemon.model";
+
 export interface User {
-    id: number,
-    username: string,
-    pokemon: string[][],
+    id: number;
+    username: string;
+    pokemon: string[];
    
 }

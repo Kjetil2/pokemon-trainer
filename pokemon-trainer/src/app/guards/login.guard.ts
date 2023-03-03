b4c1867d4defa320766b8e0df8fe200f732c5bd8
Kjetil Rønhovde <kjetil.ronhovde@no.experis.com>
2023-03-03 11:47:02 +0100

    Update: created Login guard
    Co-authored-by: Annerik <Annerik@users.noreply.github.com>

---
+++ pokemon-trainer/src/app/guards/login.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Route, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+
+  constructor(
+    private readonly router: Router,
+    private readonly userService: UserService,
+  ){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if(this.userService.user){
+      this.router.navigateByUrl('/catalogue')
+      return true;
+    }
+      return true;
+  }
+
+}
